0e16ffccfe700f03fbd09f7e4957f45b49093fec
kfs121 <kfs121@naver.com>
2023-10-30 18:00:55 +0900

    first

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  initHeight();
+  
+});
+
+function initHeight(){
+  $('section#welcome').css('height','100vh');
+  // $('section').css('width',' calc(100vw - (100vw - 100%)');
+}
